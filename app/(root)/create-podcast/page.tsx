86e2459917f7d25e3cbf8ca21be0373a05556aaa
aware <aware9758@gmail.com>
2024-08-20 15:56:02 +0200

    first

---
+++ app/(root)/create-podcast/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreatePodcast = () => {
+  return (
+    <div>
+      <h1 className="text-xl font-bold text-white-1">Create Podcasts</h1>
+    </div>
+  );
+};
+
+export default CreatePodcast;
